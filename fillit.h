d45bbe3e5d0f3b7ae51e9988dee127619672837d
Everyday Rosella <erosella@oa-b3.21-school.ru>
2019-10-19 15:16:40 +0300

    some map func

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: erosella <erosella@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 14:11:14 by erosella          #+#    #+#             */
-/*   Updated: 2019/10/13 14:35:16 by erosella         ###   ########.fr       */
+/*   Updated: 2019/10/19 15:13:46 by erosella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,25 @@
 # include <fcntl.h>
 # include "./libft/libft.h"
 
+typedef struct		s_block
+{
+	char			letter;
+	char 			*str;
+	struct t_tetris	*next;
+}					t_block;
+
+typedef struct		s_map
+{
+	char			**arr;
+}					t_map;
+
 int		valid(char *buf, int size);
 int		charcount(char *buf);
 int		adjacency_counter(char *buf);
+t_map	*new_map(int map_size);
+size_t	count_pieces(t_block *piecelist);
+void	print_map(t_map *map, int size);
+void	free_map(t_map *map, int map_size);
+
 
 #endif
